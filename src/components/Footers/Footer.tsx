3ba83a5d73c9eca488cb9650a0e8839ae7bf0f29
Marcelo Luiz Onhate <onhate@gmail.com>
2023-02-20 22:07:28 -0300

    initial page structure

---
+++ src/components/Footers/Footer.tsx
@@ -0,0 +1,42 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.3.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from 'react';
+
+// reactstrap components
+import { Container, Row } from 'reactstrap';
+
+function Footer() {
+  return (
+    <footer className="footer footer-black footer-white">
+      <Container>
+        <Row>
+          <div className="credits ml-auto">
+            <span className="copyright">
+              © {new Date().getFullYear()}, feito com {' '}
+              <i className="fa fa-heart heart" /> por Uma Bici
+            </span>
+          </div>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,15 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        <link rel="stylesheet" type="text/css" href="/css/paper-kit.css" />
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet" />
+      </Head>
       <body>
-        <Main />
-        <NextScript />
+      <Main />
+      <NextScript />
       </body>
     </Html>
-  )
+  );
 }
